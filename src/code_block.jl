32899a479b53ef1fad7ce3889d7ff264ee856c46
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-23 22:30:17 +0900

    `Base.remove_linenums!(signature)` when constructing

--- src/code_block.jl
+++ src/code_block.jl
@@ -3,10 +3,13 @@
 struct CodeBlock
     filepath::String
     signature::Expr
+    function CodeBlock(filepath::String, signature::Expr)
+        Base.remove_linenums!(signature)
+        new(filepath, signature)
+    end
 end
 
 macro code_block(filepath::Symbol, sig::Expr)
-    Base.remove_linenums!(sig)
     sig_node = QuoteNode(sig)
     quote
         CodeBlock($(esc(filepath)), $sig_node)
